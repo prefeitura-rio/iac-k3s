6f7a7bf9b8832f12cf54c87ebd69da0dd5a54d11
Pedro Mendes <pedrohrmendes@proton.me>
2025-09-01 19:34:18 -0300

    First commit.

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ansible
+            just
+            uv
+          ];
+        };
+      }
+    );
+}
