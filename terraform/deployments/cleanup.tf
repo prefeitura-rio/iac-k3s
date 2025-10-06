fa2a1f7902eb2403cb3b458700b634ab0472cef5
Pedro Mendes <pedrohrmendes@proton.me>
2025-10-06 11:11:15 -0300

    fix: add cluster role for cleanup deployment

--- terraform/deployments/cleanup.tf
+++ terraform/deployments/cleanup.tf
@@ -6,22 +6,49 @@ resource "helm_release" "kube_cleanup_operator" {
   namespace        = "kube-system"
   create_namespace = false
 
-  set_list = [
+  set = [
     {
-      name = "args"
-      value = [
-        "--legacy-mode=false",
-        "--delete-successful-after=1h",
-        "--delete-failed-after=24h",
-        "--delete-evicted-pods-after=5m",
-        "--delete-orphaned-pods-after=30m",
-        "--delete-pending-pods-after=2h"
-      ]
-    }
-  ]
+      name  = "resources.requests.memory"
+      value = "256Mi"
+    },
+    {
+      name  = "resources.limits.memory"
+      value = "256Mi"
+  }]
+
+  set_list = [{
+    name = "args"
+    value = [
+      "--legacy-mode=false",
+      "--delete-successful-after=1h",
+      "--delete-failed-after=24h",
+      "--delete-evicted-pods-after=5m",
+      "--delete-orphaned-pods-after=30m",
+      "--delete-pending-pods-after=2h"
+    ]
+  }]
 }
 
-resource "kubernetes_cluster_role_binding_v1" "kube_cleanup_operator" {
+resource "kubernetes_cluster_role" "kube_cleanup_operator" {
+  depends_on = [helm_release.kube_cleanup_operator]
+  metadata {
+    name = "kube-cleanup-operator"
+  }
+
+  rule {
+    api_groups = ["batch"]
+    resources  = ["jobs"]
+    verbs      = ["get", "list", "watch", "delete"]
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["pods"]
+    verbs      = ["get", "list", "watch", "delete"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "kube_cleanup_operator" {
   metadata {
     name = "kube-cleanup-operator"
   }
@@ -29,14 +56,12 @@ resource "kubernetes_cluster_role_binding_v1" "kube_cleanup_operator" {
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "ClusterRole"
-    name      = "cleanup-operator"
+    name      = kubernetes_cluster_role.kube_cleanup_operator.metadata[0].name
   }
 
   subject {
     kind      = "ServiceAccount"
-    name      = "kube-cleanup-operator"
-    namespace = "kube-system"
+    name      = helm_release.kube_cleanup_operator.name
+    namespace = helm_release.kube_cleanup_operator.namespace
   }
-
-  depends_on = [helm_release.kube_cleanup_operator]
 }
